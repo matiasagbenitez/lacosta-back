008cf1f3ab704d8938898a5153645c8cf9de9f5e
matiasagbenitez <matiasagbenitez@gmail.com>
2025-11-06 06:46:40 -0300

    postgres

--- src/config/database.ts
+++ src/config/database.ts
@@ -12,18 +12,18 @@ console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '***' : 'NOT SET');
 
 const sequelize = new Sequelize({
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '3306'),
+  port: parseInt(process.env.DB_PORT || '5432'),
   database: process.env.DB_NAME || 'products_db',
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Maguben237012!',
-  dialect: 'mysql',
-//   logging: process.env.NODE_ENV === 'development' ? console.log : false,
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  dialect: 'postgres',
+  logging: process.env.NODE_ENV === 'development' ? console.log : false,
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 export default sequelize;
